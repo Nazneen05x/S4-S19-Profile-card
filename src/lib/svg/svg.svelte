b6d78b9c18388093adf2bb6f233367e9dbe2ced5
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2024-09-05 23:15:26 +0200

    content

---
+++ src/lib/svg/svg.svelte
@@ -0,0 +1,40 @@
+<ul>
+    <li>
+      <img src="/heart-svgrepo-com.svg" alt="heart-svg" />
+    </li>
+    <li>
+      <img src="/message-circle-svgrepo-com.svg" alt="message-circle-svg" />
+    </li>
+    <li>
+      <img src="send-2-svgrepo-com.svg" alt="send-svg" />
+    </li>
+  
+    <li>
+      <img src="/save-svgrepo-com.svg" alt="save-svg">
+    </li>
+  
+  </ul>
+  
+  <style>
+    img {
+      width: 1.5em;
+      width: 1.5em;
+    }
+  
+    ul{
+      list-style: none;
+      display: flex;
+      gap: 0.2em;
+      position: relative;
+      bottom: 4em;
+      left: 1.5em;
+      gap: 0.3em;
+      padding: 1em;
+      
+    }
+  
+    ul li:last-child{
+      margin-left: 13rem;
+    }
+  </style>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Svg from "../lib/svg/svg.svelte";
+  </script>
+  
+  <main>
+    <section>
+      <!-- <img src="/Squad2C-Nazneen.JPG" alt="Squad2C-Nazneen" /> -->
+      <Svg />
+      <p>20 likes</p>
+      <p>First day school picture.</p>
+      <p>#Class20242025 #FDND #Github/Nazneen05x</p>
+    </section>
+  </main>
+  
+  <style>
+    main {
+      padding: 7em;
+    }
+  
+    section {
+      background-color: white;
+      width: 25em;
+      height: 34em;
+      border-radius: 0.3em;
+      margin: 0% auto;
+    }
+  
+    /* img {
+      width: 20em;
+      height: 20em;
+      padding: 2.5em;
+    } */
+  
+    p {
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      font-weight: bold;
+      font-size: small;
+      position: relative;
+      bottom: 6em;
+      margin-left: 3em;
+    }
+  
+    p:nth-of-type(2) {
+      font-weight: normal;
+      bottom: 6.5em;
+    }
+  
+    p:nth-of-type(3) {
+      font-weight: normal;
+      color: #166ff5;
+      bottom: 7em;
+    }
+  </style>
+  
+  
